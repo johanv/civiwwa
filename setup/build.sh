2921432805484745d6d6426c9887a025a13cba05
Johan Vervloet <johanv@johanv.org>
2016-12-05 22:09:06 +0100

    Build script. 1st try.
    Don't include translation zip in repository.

---
+++ setup/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DRUSH=drush8
+CHGRP=chgrp
+CHMOD=chmod
+DOCROOT=/var/www/html
+L10N=`dirname ${0}`/l10n
+
+pushd $L10N
+wget -nc https://download.civicrm.org/civicrm-4.7.13-l10n.tar.gz
+popd
+
+pushd $DOCROOT
+
+$DRUSH si minimal --db-url=mysql://dev:DbDevPw@db/drupal --db-su=root --db-su-pw=blablablaroot --site-name="CivIWWA" --locale=nl --account-name=civiwwadmin --account-mail=helpdesk@johanv.org -y
+
+mysql -h db << EOF
+drop database if exists civi;
+create database civi;
+grant all on civi.* to dev;
+EOF
+
+$DRUSH cc all
+
+$CHMOD -R ugo+rwx sites/default
+$DRUSH --include=sites/all/modules/civicrm/drupal/drush cvi --dbuser=dev --dbpass=DbDevPw --dbhost=db --dbname=civi --destination=./sites/all/modules --site_url=localhost --lang=nl_NL --langtarfile=$L10N/civicrm-4.7.13-l10n.tar.gz
+$CHMOD -R 644 sites/default
+
+APACHE_GROUP=www-data
+
+
+rm -rf sites/default/files/civicrm/templates_c/*
+rm -rf sites/default/files/civicrm/ConfigAndLog/CiviCRM.*
+
+## revert features, by design followed by double cache clear
+$DRUSH fra -y
+$DRUSH cc all
+$DRUSH cc all
+
+## TODO: enable devel.
+
+$DRUSH vset clean_url 0
+
+# chgrp inside the container, to get the correct id for www-data.
+# $CHGRP -R $APACHE_GROUP /var/www/html/sites/default/files
+# $CHMOD -R ug+rwX sites/default/files
+
+popd
