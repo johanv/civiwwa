99ee8069308d278fc53634263206236f654e288c
Johan Vervloet <johanv@johanv.org>
2017-01-20 09:19:38 +0100

    Refs #16 - Custom event fields.

---
+++ docroot/sites/all/modules/civicrm-extensions/cards.iwwa.tournament/CRM/Tournament/Upgrader.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  civiwwa.tournament - CivIWWA tournament configuration.
+  Copyright (C) 2017  Johan Vervloet
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Tournament_Upgrader extends CRM_Tournament_Upgrader_Base {
+  /**
+   * Apply configuration when module is enabled.
+   */
+  public function enable() {
+    $configResult = civicrm_api3('Civiconfig', 'load_json', [
+      // there should be a better way to do this.
+      'path' => realpath(__DIR__ . '/../../') . '/resources/'
+    ]);
+    return (!$configResult['is_error']);
+  }
+}
